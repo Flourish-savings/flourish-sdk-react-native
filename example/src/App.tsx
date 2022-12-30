31c1f7674336c7bdab37ec1bb832357618bd5c21
Rafael Kerr <rafael.kerr@gmail.com>
2022-12-30 20:58:49 -0300

    🔥  Adding Botton Navigation to Example App

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,49 @@
 import * as React from 'react';
+import { BottomNavigation, Text } from 'react-native-paper';
+import SafeAreaView from 'react-native-safe-area-view';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import HomeScreen from './HomeScreen';
+import RewardsScreen from './RewardsScreen';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'flourish-sdk-react-native';
+const RecentsRoute = () => <Text>Recents</Text>;
+
+const NotificationsRoute = () => <Text>Notifications</Text>;
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: 'music',
+      title: 'Favorites',
+      focusedIcon: 'heart',
+      unfocusedIcon: 'heart-outline',
+    },
+    { key: 'albums', title: 'Albums', focusedIcon: 'album' },
+    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
+    {
+      key: 'notifications',
+      title: 'Notifications',
+      focusedIcon: 'bell',
+      unfocusedIcon: 'bell-outline',
+    },
+  ]);
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+  const renderScene = BottomNavigation.SceneMap({
+    music: HomeScreen,
+    albums: RewardsScreen,
+    recents: RecentsRoute,
+    notifications: NotificationsRoute,
+  });
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
