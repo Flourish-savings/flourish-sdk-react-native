5202e81bfe3b5029ea62682b2bdff3752d29fb2b
Rafael Kerr <rafael.kerr@gmail.com>
2023-01-01 13:53:43 -0300

    ⚡️  get token and authenticate in Capybara

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,38 +1,46 @@
-import * as React from 'react';
-import { BottomNavigation, Text } from 'react-native-paper';
+import React, { useState } from 'react';
+import { BottomNavigation } from 'react-native-paper';
 import SafeAreaView from 'react-native-safe-area-view';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import HomeScreen from './HomeScreen';
 import RewardsScreen from './RewardsScreen';
-
-const RecentsRoute = () => <Text>Recents</Text>;
-
-const NotificationsRoute = () => <Text>Notifications</Text>;
+import FavoriteScreen from './FavoriteScreen';
+import NotificationScreen from './NotificationScreen';
 
 export default function App() {
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
     {
-      key: 'music',
+      key: 'home',
+      title: 'Home',
+      focusedIcon: 'home',
+      unfocusedIcon: 'home-outline',
+    },
+    {
+      key: 'history',
       title: 'Favorites',
       focusedIcon: 'heart',
       unfocusedIcon: 'heart-outline',
     },
-    { key: 'albums', title: 'Albums', focusedIcon: 'album' },
-    { key: 'recents', title: 'Recents', focusedIcon: 'history' },
     {
       key: 'notifications',
       title: 'Notifications',
       focusedIcon: 'bell',
       unfocusedIcon: 'bell-outline',
     },
+    {
+      key: 'rewards',
+      title: 'Rewards',
+      focusedIcon: 'gift',
+      unfocusedIcon: 'gift-outline',
+    },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
-    music: HomeScreen,
-    albums: RewardsScreen,
-    recents: RecentsRoute,
-    notifications: NotificationsRoute,
+    home: HomeScreen,
+    history: FavoriteScreen,
+    notifications: NotificationScreen,
+    rewards: RewardsScreen,
   });
 
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,59 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+import React, { useEffect } from 'react';
+import { api } from './service/api';
+
+import { StyleSheet, View, Text } from 'react-native';
+
+interface Props {
+  partnerId: string;
+  partnerSecret: string;
+  customerCode: string;
 }
+
+const Flourish: React.FC<Props> = ({
+  partnerId,
+  partnerSecret,
+  customerCode,
+}) => {
+  const authenticate = async (access_token: string): Promise<boolean> => {
+    const response = await api.signIn(access_token);
+    return response.isValid;
+  };
+
+  const getToken = async () => {
+    const response = await api.authenticate(
+      partnerId,
+      partnerSecret,
+      customerCode
+    );
+    if (response.access_token) {
+      authenticate(response.access_token);
+    } else {
+      console.error(`Error fetching token: ${JSON.stringify(response)}`, []);
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text>Thw WebView will be rendered here</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
+
+export default Flourish;
