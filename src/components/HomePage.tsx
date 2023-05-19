0105d2ccda4954678b45160d98f0903581725905
Rafael Kerr <rafael.kerr@gmail.com>
2023-05-18 23:20:38 -0300

    ⚡️ Adding conditional rendering to webview component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { emitEvent } from '../events/eventManager';
 import { WebView } from 'react-native-webview';
 import Config from '../config';
@@ -10,29 +10,36 @@ type Props = {
 };
 
 const HomePage = (props: Props) => {
-  const baseURL = Config.FRONTEND_API_URL.get(props.environment);
-  const endURL = `/?token=${props.token}`;
-  const completeURL = `${baseURL}${props.language}${endURL}`;
-  console.log('completeURL', completeURL);
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const baseURL = Config.FRONTEND_API_URL.get(props.environment);
+    const endURL = `/?token=${props.token}`;
+    const completeURL = `${baseURL}${props.language}${endURL}`;
+    if (baseURL !== undefined && props.token !== undefined) setUrl(completeURL);
+  }, [props.environment, props.language, props.token]);
 
   return (
-    // @ts-ignore
-    <WebView
-      source={{
-        uri: `${completeURL}`,
-      }}
-      // eslint-disable-next-line react-native/no-inline-styles
-      style={{
-        marginTop: 45,
-      }}
-      javaScriptEnabled={true}
-      onMessage={(event) => {
-        console.log(event);
-        const data = JSON.parse(event.nativeEvent.data);
-        console.log('Event in WebView Component', data);
-        emitEvent(data);
-      }}
-    />
+    <>
+      {url !== '' && (
+        <WebView
+          source={{
+            uri: `${url}`,
+          }}
+          // eslint-disable-next-line react-native/no-inline-styles
+          style={{
+            marginTop: 45,
+          }}
+          javaScriptEnabled={true}
+          onMessage={(event) => {
+            console.log(event);
+            const data = JSON.parse(event.nativeEvent.data);
+            console.log('Event in WebView Component', data);
+            emitEvent(data);
+          }}
+        />
+      )}
+    </>
   );
 };
 
