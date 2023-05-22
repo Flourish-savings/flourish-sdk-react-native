4d1f6a10c958b391084d0dcdf1c9a3595b6e939b
Rafael Kerr <rafael.kerr@gmail.com>
2023-05-22 08:38:49 -0300

    🚀  Refactoring state management

--- src/events/eventManager.ts
+++ src/events/eventManager.ts
@@ -1,15 +1,25 @@
 import { EventRegister } from 'react-native-event-listeners';
+import { sdkStore } from '../index';
+import { authenticate } from '../index';
 
 export const emitConfig = { GENERIC_EVENT: 'GENERIC_EVENT' };
 
 export const emitEvent = (event: any) => {
-  console.log('Event in emitEvent/EventManager', event);
+  if (event?.data?.code === 'FE0003') {
+    const { customerCode } = sdkStore.getState();
+    const userCode: string = customerCode !== undefined ? customerCode : '';
+    sdkStore.setState({
+      ...sdkStore.getState(),
+      isError: true,
+    });
+    authenticate(userCode);
+  }
   EventRegister.emit(emitConfig.GENERIC_EVENT, event);
 };
 
 export const onEventReceived = (eventCallback: (arg: any) => any) => {
+  EventRegister.removeAllListeners();
   EventRegister.addEventListener(emitConfig.GENERIC_EVENT, (data) => {
-    console.log('Event in onEventReceived/EventManager', data);
     eventCallback(data);
   });
 };
