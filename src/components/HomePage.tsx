5406ac9a8c3c578c288419a3ac5e6d4e696c9d2a
dTravassosFlourish <diogo@flourishfi.com>
2024-04-22 18:47:08 -0300

    CPBR-2110 fix CI

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -16,7 +16,9 @@ const HomePage = (props: Props) => {
     const baseURL = Config.FRONTEND_API_URL.get(props.environment);
     const tokenPath = `?token=${props.token}`;
     const languagePath = `&lang=${props.language}`;
-    const versionPath = `&sdk_version=${Config.FLOURISH_SDK_APP_VERSION.get(props.environment)}`
+    const versionPath = `&sdk_version=${
+      Config.FLOURISH_SDK_APP_VERSION.get(props.environment) as string
+    }`;
     const completeURL = `${baseURL}${tokenPath}${languagePath}${versionPath}`;
     if (baseURL !== undefined && props.token !== undefined) setUrl(completeURL);
   }, [props.environment, props.language, props.token]);
