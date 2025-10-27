40980f7ad260243e354499465a912d65faa41470
Rafael Kerr <rafael.kerr@gmail.com>
2025-10-27 01:01:18 -0300

    Creating new expo app example

--- src/config/config.production.ts
+++ src/config/config.production.ts
@@ -1,5 +1,5 @@
 export default {
   FRONTEND_API_URL: 'https://platform.flourishfi.com',
   BACKEND_API_URL: 'https://auth.flourishfi.com/api/v3',
-  FLOURISH_SDK_APP_VERSION: '3.1.1',
+  FLOURISH_SDK_APP_VERSION: '3.2.0',
 };
