e592d3f185d761d6482dd9cabe05c014e7c767ed
Diogo Flourish <flourish-loaner@Flourishs-MacBook-Pro.local>
2025-10-28 12:37:18 -0300

    adjust version

--- src/config/config.staging.ts
+++ src/config/config.staging.ts
@@ -1,5 +1,5 @@
 export default {
   FRONTEND_API_URL: 'https://platform-stg.flourishfi.com',
   BACKEND_API_URL: 'https://auth-stg.flourishfi.com/api/v3',
-  FLOURISH_SDK_APP_VERSION: '3.2.0',
+  FLOURISH_SDK_APP_VERSION: '3.2.1',
 };
